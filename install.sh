060f4a47ff154889a8cbbcb78b542a1ab6e2afa2
Phillip LeBlanc <phillip@leblanc.tech>
2024-12-19 14:31:30 +0900

    Add moved notice to all samples (#143)
    * Add moved notice to all samples
    
    * Remove .DS_Store

--- install.sh
+++
@@ -1,3 +0,0 @@
-
-#!/bin/sh
-curl https://raw.githubusercontent.com/spiceai/spiceai/trunk/install/install.sh?token=AAATSLQSDXBE3XMLCOCU6ZTBFRO7U | /bin/bash
--- sales-bi/superset/init/import-dashboards.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CONTAINER_NAME="superset-sales-bi-demo"
-
-docker exec -it $CONTAINER_NAME superset import-dashboards \
-              --path /dashboard/dashboard-sales-demo.zip \
-              --username admin
